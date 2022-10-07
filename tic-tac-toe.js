1331b0b9d1587de52894fcbd806f3f6e4c21fc21
Kevin McBean <kevin.mcbean@mymona.uwi.edu>
2022-10-07 02:27:49 -0500

    X and O show up

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,4 +1,30 @@
 
+gamestatus = [];
+
+function makeMove(){
+if(gamestatus[gamestatus.length-1]==="X"){
+  event.target.classList.add("O");
+  event.target.textContent = "O";
+  gamestatus.push("O");
+}
+else if(gamestatus[gamestatus.length-1]==="O"){
+  event.target.classList.add("X");
+  event.target.textContent = "X";
+  gamestatus.push("X");
+}
+else if(gamestatus[gamestatus.length-1]==="XWON"){
+  alert("X WON!");
+}
+else if(gamestatus[gamestatus.length-1]==="OWON"){
+  alert("O WON!");
+}
+else{
+  event.target.classList.add("X");
+  event.target.textContent = "X";
+  gamestatus.push("X");
+
+}
+}
 
 document.addEventListener('DOMContentLoaded', () => {
             let btn = document.body.getElementsByClassName('btn')[0];
@@ -8,6 +34,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
                 for (square of squares){
                   square.classList.add('square');
+                  square.addEventListener("click",makeMove)
                 }
             });
         });
